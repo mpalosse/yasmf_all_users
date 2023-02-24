ebc2334f5def3d982b6673ff8523402b468a031a
Franck Silvestre <franck.silvestre@gmail.com>
2023-02-24 07:42:27 +0100

    Some tests experimentation

--- all_users/index.php
+++ all_users/index.php
@@ -8,8 +8,8 @@ $data_source = new DataSource(
     $host = 'localhost',
     $port = 8889, # to change with the port your mySql server listen to
     $db_name = 'all_users', # to change with your db name
-    $user = 'username', # to change with your db username
-    $pass = '*******', # to change with your db password
+    $user = 'root', # to change with your db username
+    $pass = 'root', # to change with your db password
     $charset = 'utf8mb4'
 );
 
