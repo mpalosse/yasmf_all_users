10a9eacfc4714984bc6cd976ad25f158335d81f4
Franck Silvestre <franck.silvestre@gmail.com>
2023-02-24 14:54:53 +0100

    test component factory

---
+++ tests/application/DefaultComponentFactoryTest.php
@@ -0,0 +1,69 @@
+<?php
+/*
+ * yasmf - Yet Another Simple MVC Framework (For PHP)
+ *     Copyright (C) 2023   Franck SILVESTRE
+ *
+ *     This program is free software: you can redistribute it and/or modify
+ *     it under the terms of the GNU Affero General Public License as published
+ *     by the Free Software Foundation, either version 3 of the License, or
+ *     (at your option) any later version.
+ *
+ *     This program is distributed in the hope that it will be useful,
+ *     but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *     GNU Affero General Public License for more details.
+ *
+ *     You should have received a copy of the GNU Affero General Public License
+ *     along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+namespace application;
+
+use controllers\HomeController;
+use PHPUnit\Framework\TestCase;
+use services\UsersService;
+use yasmf\NoControllerAvailableForName;
+use yasmf\NoServiceAvailableForName;
+
+class DefaultComponentFactoryTest extends TestCase
+{
+
+    private DefaultComponentFactory $componentFactory;
+
+    public function setUp(): void
+    {
+        parent::setUp();
+        // given a component factory
+        $this->componentFactory = new DefaultComponentFactory();
+    }
+
+    public function testBuildControllerByName_Home()
+    {
+        // when ask for home controller
+        $controller = $this->componentFactory->buildControllerByName("Home");
+        // then the controller is HomeController instance
+        self::assertInstanceOf(HomeController::class,$controller);
+    }
+
+    public function testBuildControllerByName_Other()
+    {
+        // expected exception when ask for a non-existant controller
+        $this->expectException(NoControllerAvailableForName::class);
+        $controller = $this->componentFactory->buildControllerByName("NoController");
+    }
+
+    public function testBuildServiceByName_Users()
+    {
+        // when ask for user service
+        $service = $this->componentFactory->buildServiceByName("Users");
+        // then the service is UsersService instance
+        self::assertInstanceOf(UsersService::class,$service);
+    }
+
+    public function testBuildServiceByName_Other()
+    {
+        // expected exception when ask for a non-existant service
+        $this->expectException(NoServiceAvailableForName::class);
+        $this->componentFactory->buildServiceByName("NoService");
+    }
+}
